68047087dc79c7cfb75fab6e07730e1a20a4ad44
ikiboy <omega2707@yahoo.com>
2024-05-24 22:30:47 +0200

    Cleaned App file

--- App.tsx
+++ App.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import type { PropsWithChildren } from 'react';
 import {
-  Alert,
-  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { AsyncStorageKey, getAsyncStorage, removeAsyncStorage, setAsyncStorage } from './src/config/asyncstorageConfig';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import IonIcon from 'react-native-vector-icons/Ionicons';
-
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -44,60 +26,6 @@ function App(): React.JSX.Element {
         style={backgroundStyle}
         contentContainerStyle={{ padding: 10, gap: 10 }}
       >
-        <Text>Hello</Text>
-        <Button
-          title='SAVE'
-          onPress={async () => {
-            try {
-              setAsyncStorage(AsyncStorageKey.FOOD, 'BECKA SNICLA');
-            } catch (e) {
-              // saving error
-            }
-          }}
-        />
-
-        <Button
-          title='GET'
-          onPress={async () => {
-            try {
-              const value = await getAsyncStorage(AsyncStorageKey.FOOD);
-              if (value !== null) {
-                Alert.alert(value ?? 'nedefinisano');
-              }
-              else {
-                Alert.alert('SIPAK');
-              }
-            } catch (e) {
-              // error reading value
-            }
-          }}
-        />
-
-        <Button
-          title='CLEAR'
-          onPress={async () => {
-            try {
-              removeAsyncStorage(AsyncStorageKey.FOOD);
-            } catch (e) {
-              // saving error
-            }
-          }}
-        />
-
-        {/* Icons: https://pictogrammers.com/library/mdi/ */}
-        <Icon name="flower" size={100} color="blue" />
-        <IonIcon name="airplane-outline" size={100} color="blue" />
-        <View style={{width:200}}>
-          <Icon.Button
-            name="facebook"
-            backgroundColor="#3b5998"
-            style={{ width: 200, }}
-
-          // onPress={this.loginWithFacebook}
-          >
-            Login with Facebook
-          </Icon.Button>
-        </View>
       </ScrollView>
     </SafeAreaView>
   );
